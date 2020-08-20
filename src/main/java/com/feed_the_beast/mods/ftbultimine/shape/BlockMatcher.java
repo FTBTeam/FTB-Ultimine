177e02e557bfedf828723fcf764ade54d0e96da5
LatvianModder <latvianmodder@gmail.com>
2020-08-20 10:13:55 +0300

    Fixed crash with tags. Probably.

--- src/main/java/com/feed_the_beast/mods/ftbultimine/shape/BlockMatcher.java
+++ src/main/java/com/feed_the_beast/mods/ftbultimine/shape/BlockMatcher.java
@@ -3,7 +3,7 @@ package com.feed_the_beast.mods.ftbultimine.shape;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BushBlock;
 import net.minecraft.block.CropsBlock;
-import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.Tags;
 
 /**
  * @author LatvianModder
@@ -13,9 +13,6 @@ public interface BlockMatcher
 	boolean check(BlockState original, BlockState state);
 
 	BlockMatcher MATCH = (original, state) -> original.getBlock() == state.getBlock();
-
-	ResourceLocation STONE_TAG = new ResourceLocation("forge", "stone");
-	BlockMatcher ANY_STONE = (original, state) -> state.getBlock().getTags().contains(STONE_TAG);
-
+	BlockMatcher ANY_STONE = (original, state) -> Tags.Blocks.STONE.contains(state.getBlock());
 	BlockMatcher BUSH = (original, state) -> state.getBlock() instanceof BushBlock && (original.getBlock() instanceof CropsBlock == state.getBlock() instanceof CropsBlock);
 }
