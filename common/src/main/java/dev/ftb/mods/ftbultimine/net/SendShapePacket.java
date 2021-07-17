39390fce60171176d1f14c6f4cc74148b08ab480
Max <maxh2709@gmail.com>
2021-07-17 20:00:52 +0200

    Fix old shape lingering if new shape is above render threshold

--- common/src/main/java/dev/ftb/mods/ftbultimine/net/SendShapePacket.java
+++ common/src/main/java/dev/ftb/mods/ftbultimine/net/SendShapePacket.java
@@ -2,13 +2,13 @@ package dev.ftb.mods.ftbultimine.net;
 
 import dev.ftb.mods.ftbultimine.FTBUltimine;
 import dev.ftb.mods.ftbultimine.config.FTBUltimineClientConfig;
-import dev.ftb.mods.ftbultimine.config.FTBUltimineServerConfig;
 import dev.ftb.mods.ftbultimine.shape.Shape;
 import me.shedaniel.architectury.networking.NetworkManager;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -49,8 +49,10 @@ public class SendShapePacket {
 		context.get().queue(() -> {
 			current = shape;
 
-			if (blocks.size() < FTBUltimineClientConfig.renderOutline.get()) {
+			if (blocks.size() <= FTBUltimineClientConfig.renderOutline.get()) {
 				FTBUltimine.instance.proxy.setShape(blocks);
+			} else {
+				FTBUltimine.instance.proxy.setShape(Collections.emptyList());
 			}
 		});
 	}
