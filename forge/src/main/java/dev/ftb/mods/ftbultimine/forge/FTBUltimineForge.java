cdf50f2251685895f76e46825df8a09accbde2a9
Des Herriott <des.herriott@gmail.com>
2023-01-31 11:13:42 +0000

    chore: remove deprecated forge RenderLevelLastEvent
    RenderLevelStageEvent is the new way, rendering after TRANSLUCENT phase

--- forge/src/main/java/dev/ftb/mods/ftbultimine/forge/FTBUltimineForge.java
+++ forge/src/main/java/dev/ftb/mods/ftbultimine/forge/FTBUltimineForge.java
@@ -3,7 +3,7 @@ package dev.ftb.mods.ftbultimine.forge;
 import dev.ftb.mods.ftbultimine.FTBUltimine;
 import dev.ftb.mods.ftbultimine.event.LevelRenderLastEvent;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RenderLevelLastEvent;
+import net.minecraftforge.client.event.RenderLevelStageEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
@@ -23,8 +23,10 @@ public class FTBUltimineForge {
 
 	private static class FTBUltimineForgeClient {
 		static void init() {
-			MinecraftForge.EVENT_BUS.<RenderLevelLastEvent>addListener(event -> {
-				LevelRenderLastEvent.EVENT.invoker().onRenderLast(event.getPoseStack());
+			MinecraftForge.EVENT_BUS.<RenderLevelStageEvent>addListener(event -> {
+				if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_TRANSLUCENT_BLOCKS) {
+					LevelRenderLastEvent.EVENT.invoker().onRenderLast(event.getPoseStack());
+				}
 			});
 		}
 	}
