7ffe2cf3cb9072ce9b92e6c31f88a34dcf9f3445
Des Herriott <des.herriott@gmail.com>
2023-04-03 14:07:08 +0100

    feat: right-click harvest support for cocoa beans and sweet berries
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/742

--- common/src/main/java/dev/ftb/mods/ftbultimine/shape/BlockMatcher.java
+++ common/src/main/java/dev/ftb/mods/ftbultimine/shape/BlockMatcher.java
@@ -2,10 +2,7 @@ package dev.ftb.mods.ftbultimine.shape;
 
 import dev.ftb.mods.ftbultimine.FTBUltimine;
 import dev.ftb.mods.ftbultimine.config.FTBUltimineServerConfig;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.BushBlock;
-import net.minecraft.world.level.block.CropBlock;
-import net.minecraft.world.level.block.SaplingBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 
 /**
@@ -21,13 +18,16 @@ public interface BlockMatcher {
 	BlockMatcher MATCH = (original, state) -> original.getBlock() == state.getBlock();
 	BlockMatcher TAGS_MATCH_SHAPELESS = FTBUltimineServerConfig.MERGE_TAGS_SHAPELESS::match;
 	BlockMatcher TAGS_MATCH_SHAPED = FTBUltimineServerConfig.MERGE_TAGS_SHAPED::match;
-	BlockMatcher BUSH = (original, state) -> state.getBlock() instanceof BushBlock && getBushType(state.getBlock()) == getBushType(original.getBlock());
+	BlockMatcher CROP_LIKE = (original, state) -> (state.getBlock() instanceof BushBlock || state.getBlock() instanceof CocoaBlock)
+			&& getBushType(state.getBlock()) == getBushType(original.getBlock());
 
 	static int getBushType(Block block) {
 		if (block instanceof CropBlock) {
 			return 1;
 		} else if (block instanceof SaplingBlock) {
 			return 2;
+		} else if (block instanceof CocoaBlock) {
+			return 3;
 		}
 
 		return 0;
